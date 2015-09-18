2ea3c4854ccaf14ce274d3b90f10e80ad94abaa8
Federico Tomassetti <federico@tomassetti.me>
2015-09-18 09:53:26 +0200

    preparing more functional tests

--- functional_tests/features/step_definitions/post_steps.rb
+++ functional_tests/features/step_definitions/post_steps.rb
@@ -5,8 +5,19 @@ require 'json'
 require "rspec"
 include RSpec::Matchers
 
-
 # psql -h 127.0.0.1 -p 7500 -U blog_owner -d blog
+
+#
+# Given
+#
+
+Given(/^that on the DB there is a post with UUID=(\d+)ff(\d+)\-(\d+)e\-(\d+)\-a(\d+)\-(\d+)ef(\d+) title="([^"]*)" content="([^"]*)"$/) do |arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+#
+# When
+#
 
 When(/^I insert a post with title "([^"]*)" and content "([^"]*)"$/) do |title, content|
   payload = """
@@ -19,6 +30,22 @@ When(/^I insert a post with title "([^"]*)" and content "([^"]*)"$/) do |title, 
   response = RestClient.post 'http://localhost:4567/posts', payload, :content_type => :json, :accept => :json
   expect(response.code).to eq(201)
 end
+
+When(/^I edit post (\d+)ff(\d+)\-(\d+)e\-(\d+)\-a(\d+)\-(\d+)ef(\d+) setting title="([^"]*)"$/) do |arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I edit post (\d+)ff(\d+)\-(\d+)e\-(\d+)\-a(\d+)\-(\d+)ef(\d+) setting content="([^"]*)"$/) do |arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I delete post (\d+)ff(\d+)\-(\d+)e\-(\d+)\-a(\d+)\-(\d+)ef(\d+)$/) do |arg1, arg2, arg3, arg4, arg5, arg6, arg7|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+#
+# Then
+#
 
 Then(/^I have (\d+) posts?$/) do |n_posts|
     begin
@@ -54,4 +81,16 @@ Then(/^the post has content "([^"]*)"$/) do |content|
         STDERR.puts (e.methods)
         throw e
     end
+end
+
+Then(/^the post (\d+)ff(\d+)\-(\d+)e\-(\d+)\-a(\d+)\-(\d+)ef(\d+) has title "([^"]*)"$/) do |arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the post (\d+)ff(\d+)\-(\d+)e\-(\d+)\-a(\d+)\-(\d+)ef(\d+) has content "([^"]*)"$/) do |arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^post (\d+)ff(\d+)\-(\d+)e\-(\d+)\-a(\d+)\-(\d+)ef(\d+) is not found$/) do |arg1, arg2, arg3, arg4, arg5, arg6, arg7|
+  pending # Write code here that turns the phrase above into concrete actions
 end
