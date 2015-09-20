a9ed00eb088b1f2c81e5c83e92fc08b881a7844c
Federico Tomassetti <federico@tomassetti.me>
2015-09-20 09:53:01 +0200

    remove extra logging from start_application.sh

--- functional_tests/start_application.sh
+++ functional_tests/start_application.sh
@@ -1,4 +1,3 @@
-echo "start application: start"
 mvn -f ../pom.xml exec:java -Dexec.args="--db-port 7500" > log_app_out.txt 2> log_app_err.txt &
 echo $! > .saved_pid
-echo "start application: end"
+
