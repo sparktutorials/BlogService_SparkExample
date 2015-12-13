08521f2e28e52548023adb26dbc6dcda1214c790
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2015-12-13 11:30:49 +0530

    Updated to latest version Jars and fixed code issues

--- src/main/java/me/tomassetti/BlogService.java
+++ src/main/java/me/tomassetti/BlogService.java
@@ -21,7 +21,7 @@ import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.Spark.put;
 import static spark.Spark.delete;
-import static spark.SparkBase.port;
+import static spark.Spark.port;
 
 public class BlogService 
 {
